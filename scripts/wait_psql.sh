273416943053cdd1358dc42ed97669ee3f7b7224
Breno <brenolimanf@hotmail.com>
2025-11-07 20:53:00 -0300

    app criado

---
+++ scripts/wait_psql.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+until pg_isready -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER; do
+  echo "🟡 Waiting for Postgres Database Startup ($POSTGRES_HOST:$POSTGRES_PORT)..."
+  sleep 2
+done
+
+echo "✅ Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)"
