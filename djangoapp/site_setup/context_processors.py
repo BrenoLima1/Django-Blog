173c802d89756772a388654fd12c0cf75a40eb41
Breno <brenolimanf@hotmail.com>
2025-11-09 20:36:48 -0300

    criadas novas models

--- djangoapp/site_setup/context_processors.py
+++ djangoapp/site_setup/context_processors.py
@@ -8,13 +8,8 @@ def context_processor_example(request):
 
 
 def site_setup(request):
-    """Return the latest SiteSetup instance as `site_setup`.
-
-    The template expects `site_setup` to be the model instance so
-    templates can use `site_setup.favicon.url`. Previously this returned
-    a dict which prevented access to attributes like `.favicon`.
-    """
     setup = SiteSetup.objects.order_by('-id').first()
+
     return {
         'site_setup': setup
     }
